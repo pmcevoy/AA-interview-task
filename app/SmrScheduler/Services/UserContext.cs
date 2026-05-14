7811c681c417479647399213cac47db5f1a243ed
Peter McEvoy <peter.m.mcevoy@gmail.com>
2026-05-14 12:20:06 +0100

    Changing Act-As was not forcing mechanic page to reload

--- app/SmrScheduler/Services/UserContext.cs
+++ app/SmrScheduler/Services/UserContext.cs
@@ -5,4 +5,7 @@ public class UserContext
     public int? MechanicId { get; set; }
     public string? MechanicName { get; set; }
     public bool IsAdmin => MechanicId == null;
+
+    public event Action? OnChange;
+    public void NotifyChanged() => OnChange?.Invoke();
 }
